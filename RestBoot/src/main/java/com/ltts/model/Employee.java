39ef2a0fee7d735e8ce224371cf5f4f044870670
Madhusudhan <madhu.jc111@gmail.com>
2021-04-03 14:32:59 +0530

    Created curd operations using SpringBoot

---
+++ RestBoot/src/main/java/com/ltts/model/Employee.java
@@ -0,0 +1,73 @@
+package com.ltts.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private long id;
+	@Column(name = "first_name")
+	private String firstName;
+	@Column(name = "last_name")
+	private String lastName;
+	@Column(name = "email_Id") 
+	private String emailId;
+
+	public Employee() {
+
+	}
+
+	public Employee(long id, String firstName, String lastName, String emailId) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.emailId = emailId;
+	}
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFirstname() {
+		return firstName;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstName = firstname;
+	}
+
+	public String getLastname() {
+		return lastName;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastName = lastname;
+	}
+
+	public String getEmialId() {
+		return emailId;
+	}
+
+	public void setEmialId(String emialId) {
+		this.emailId = emialId;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", firstname=" + firstName + ", lastname=" + lastName + ", emialId=" + emailId
+				+ "]";
+	}
+}
